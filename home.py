6c318b9529eaf91e6a9f356d1bc6290edbea5c3a
Oswald BENOIT <oswaldbenoit05@gmail.com>
2023-06-06 01:00:19 +0200

    fifth_commit

--- home.py
+++ home.py
@@ -1,14 +1,9 @@
 import streamlit as st
-
+from PIL import Image
 # Titre en grand
 st.title("Paris respire, is it efficient ?")
 
-# Sélection de l'image
-image_path = "C:\Users\Oswald Benoit\Desktop\PSB\cours\Business_Intelligence\streamlit_Paris_Respire\data\image_paris_respire.jpeg"
-image = st.image(image_path, caption="Image de votre choix", use_column_width=True)
-
-# Instructions supplémentaires
-st.write("Voici une image de votre choix. Vous pouvez personnaliser le titre et l'image en modifiant les valeurs dans le code.")
-
-# Lien vers votre source de données, etc.
-st.write("Source des données : [Lien vers la source des données](https://www.example.com)")
+# Load image from file using Pillow
+image = Image.open("image_paris_respire.jpeg")
+# Display image using Streamlit
+st.image(image, caption='Paris Respire Image')
